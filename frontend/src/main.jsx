6d8dfb0caeb07b82770472821212aee4be8b7792
Sauravkumar903 <sauravshubham903@gmail.com>
2026-04-01 17:55:10 +0530

    Fix Google OAuth: Remove conflicting GoogleOAuthProvider, use Firebase Auth only

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -42,23 +42,16 @@ import { Provider } from "react-redux";
 import store, { persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
-// Google Auth
-import { GoogleOAuthProvider } from "@react-oauth/google";
-
 // UI
 import { Toaster } from "./components/ui/sonner.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <GoogleOAuthProvider
-      clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID || ""}
-    >
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <App />
-          <Toaster />
-        </PersistGate>
-      </Provider>
-    </GoogleOAuthProvider>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
